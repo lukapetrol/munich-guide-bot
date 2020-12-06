96a1eee40a2945b14a05c179822d5a7c7f8eb652
lukapetrol <luka.petrovic@gmx.de>
2020-12-06 05:38:28 +0100

    client_secret.json removed

--- data.js
+++ data.js
@@ -15,43 +15,7 @@ const creds = {
     "client_x509_cert_url": process.env.GOOGLE_CLIENT_CERT_URL
 }
 
-
-
-const creds2 = require("./client_secret.json");
-
-function test() {
-    var credKeys = Object.keys(creds1);
-    for(key of credKeys) {
-        if(creds1[key] !== creds2[key]) {
-            console.log(creds1[key]);
-            console.log(creds2[key]);
-        }
-    }
-}
-
 const doc = new GoogleSpreadsheet("15xbstTjUU1-xa6GYPZue57UKHbGsbFG2qyWiDhi-IB0");
-
-// "type": "service_account",
-// "project_id": "testnode-296622",
-// "private_key_id": "7160c22c114ef56d518518a774f9931453d54de4",
-// "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCoc6hbYND70Nye\n6Em5nzkaCPW7QQSsi46TVH6y6POyaIhsXWBnP3zcGWnGZsG0OULJ5RE9Xdu1uXV2\nYWHcbzAVCLDOSloLLCtsf7ren/g8zsKg/GZE+k3Edn0qzejb/IJVxu7l1/OZpTUu\n4pyaqF1QxnCyb90zHHKHIVWj0x4h9pfogMiBQdca57NmkPaoRl9hYzLzRk8cOCcF\nEbyDz9wRpMALHkcVo4/aY13n3nDem8J53DhhyHrXu/mfWXw/mnJqOIKpaWs8O67Y\nosYYr0UhLRubg7eu2AePtnRHJwKLetAVfCH+hYVQcSQDv4eto4G91lhfd3LNjvFC\ndXG83kZFAgMBAAECggEAFi4IXw5xOz7l1bc7LImLq2MToUtvJ0hG28ehLm3K11UO\nkB9+t97dp14MleGQzr9IDS0FUexfp16whIelADEpsRNQXbvffZ9xZkYD38Kgx7eS\nW+pv4tQxqip50kmDcs0dh4LRO4qYL2+tKtZkDV8ZeQRv8VFrsYpPhX28+NGtottO\nWoqOwJctSfXRHakIuNgy3HGQRXArr44fKS6AKugxiiGwuggLTiRtXfC4aFMxKdAI\ndrPwTPgnf81wVpku2+dyHPprLIq/RmO3w4vXsEFgeNxVNYMDpqMhKoiL5H5yAoPu\nrdDOZ8SkZIWaD3xGR9Ckj25IfaCHnz5KnojAd0Kk/wKBgQDWfW6XugmEti3R19Re\nzVTepOY+OGAu4ZCi6MQqhaomGBgRVDzsrDUpYV/IHxuSaTayAaYiHCk/ocGZQyJq\n0KIPDcoWMWlbjJd5MMTLFjKm4SsM/KDY//zikjSrbcIozwbBJac9YhOnB783k8VN\nANWGXHs7dZLz4fTkvpvrmCwMGwKBgQDJDVYebgii0PAKYn56skR+orb+JPCVsC9p\n056TOMNgdJnI2qpPq/8ox48tTmM1aXKz78qQffxAp+9it7ZARRYkuMiB6L3Q0/Lr\nyE38T+kLLfiwsfSSOOCokVSZZ+IynTsRSv5G0OTzd1YcuPe6sxYcU/xavzYWRbop\nkGSIaZldHwKBgQDVUlLXnDMzRm0zstCIqParqJ1FbJRcjAJpw86169JZqMTnFzyw\ntmT5DttQEYA+kV3QlEtCyXdMzffaEib8zRsglbpTcDk00BMmeRaAVUdzKabDd84R\nhCdiv76acMMWCzDQkD5QkmaxTMck41brStUSmJAHhtvsquIBToGMQwAr2QKBgAkt\nwrDA9UP5pfvTY1LWsKnXY+NWbGVpFcEPGD5AaSsOJ7czxHK+t3d1JOQ8n+rgVYxB\ndk4CcnHBOPyYxolIniovBXho18Gg64Y4LtdGDq2j+6aVeUTzs83+VI5sX7dP1lKD\nyfgRsWIsIJJPH4A2jNveObcq451Asa8UGILhz06hAoGAUPNSaPQXeR4mI96TTLjE\nZ0rrT6+PlrKUMw+qq8VX/mlOZx41c9/u5ZmHQBWczS+Nal3vazmQYYNGlRhSbIwp\nRpEDOiVvfd+qVPgLgjzY/ryF23FXcLUsKZts/8jRNUZA7IU4fYLRdA34ruA9V/ti\n1HlV3KyyehiACq5Z9lizU1Y=\n-----END PRIVATE KEY-----\n",
-// "client_email": "munich-guide-bot@testnode-296622.iam.gserviceaccount.com",
-// "client_id": "100396051393634173307",
-// "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-// "token_uri": "https://oauth2.googleapis.com/token",
-// "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-// "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/munich-guide-bot%40testnode-296622.iam.gserviceaccount.com"
-
-// GOOGLE_ACCOUNT_TYPE=service_account
-// GOOGLE_PROJECT_ID=testnode-296622
-// GOOGLE_PRIVATE_KEY_ID=7160c22c114ef56d518518a774f9931453d54de4
-// GOOGLE_PRIVATE_KEY=-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCoc6hbYND70Nye\n6Em5nzkaCPW7QQSsi46TVH6y6POyaIhsXWBnP3zcGWnGZsG0OULJ5RE9Xdu1uXV2\nYWHcbzAVCLDOSloLLCtsf7ren/g8zsKg/GZE+k3Edn0qzejb/IJVxu7l1/OZpTUu\n4pyaqF1QxnCyb90zHHKHIVWj0x4h9pfogMiBQdca57NmkPaoRl9hYzLzRk8cOCcF\nEbyDz9wRpMALHkcVo4/aY13n3nDem8J53DhhyHrXu/mfWXw/mnJqOIKpaWs8O67Y\nosYYr0UhLRubg7eu2AePtnRHJwKLetAVfCH+hYVQcSQDv4eto4G91lhfd3LNjvFC\ndXG83kZFAgMBAAECggEAFi4IXw5xOz7l1bc7LImLq2MToUtvJ0hG28ehLm3K11UO\nkB9+t97dp14MleGQzr9IDS0FUexfp16whIelADEpsRNQXbvffZ9xZkYD38Kgx7eS\nW+pv4tQxqip50kmDcs0dh4LRO4qYL2+tKtZkDV8ZeQRv8VFrsYpPhX28+NGtottO\nWoqOwJctSfXRHakIuNgy3HGQRXArr44fKS6AKugxiiGwuggLTiRtXfC4aFMxKdAI\ndrPwTPgnf81wVpku2+dyHPprLIq/RmO3w4vXsEFgeNxVNYMDpqMhKoiL5H5yAoPu\nrdDOZ8SkZIWaD3xGR9Ckj25IfaCHnz5KnojAd0Kk/wKBgQDWfW6XugmEti3R19Re\nzVTepOY+OGAu4ZCi6MQqhaomGBgRVDzsrDUpYV/IHxuSaTayAaYiHCk/ocGZQyJq\n0KIPDcoWMWlbjJd5MMTLFjKm4SsM/KDY//zikjSrbcIozwbBJac9YhOnB783k8VN\nANWGXHs7dZLz4fTkvpvrmCwMGwKBgQDJDVYebgii0PAKYn56skR+orb+JPCVsC9p\n056TOMNgdJnI2qpPq/8ox48tTmM1aXKz78qQffxAp+9it7ZARRYkuMiB6L3Q0/Lr\nyE38T+kLLfiwsfSSOOCokVSZZ+IynTsRSv5G0OTzd1YcuPe6sxYcU/xavzYWRbop\nkGSIaZldHwKBgQDVUlLXnDMzRm0zstCIqParqJ1FbJRcjAJpw86169JZqMTnFzyw\ntmT5DttQEYA+kV3QlEtCyXdMzffaEib8zRsglbpTcDk00BMmeRaAVUdzKabDd84R\nhCdiv76acMMWCzDQkD5QkmaxTMck41brStUSmJAHhtvsquIBToGMQwAr2QKBgAkt\nwrDA9UP5pfvTY1LWsKnXY+NWbGVpFcEPGD5AaSsOJ7czxHK+t3d1JOQ8n+rgVYxB\ndk4CcnHBOPyYxolIniovBXho18Gg64Y4LtdGDq2j+6aVeUTzs83+VI5sX7dP1lKD\nyfgRsWIsIJJPH4A2jNveObcq451Asa8UGILhz06hAoGAUPNSaPQXeR4mI96TTLjE\nZ0rrT6+PlrKUMw+qq8VX/mlOZx41c9/u5ZmHQBWczS+Nal3vazmQYYNGlRhSbIwp\nRpEDOiVvfd+qVPgLgjzY/ryF23FXcLUsKZts/8jRNUZA7IU4fYLRdA34ruA9V/ti\n1HlV3KyyehiACq5Z9lizU1Y=\n-----END PRIVATE KEY-----\n
-// GOOGLE_CLIENT_EMAIL=munich-guide-bot@testnode-296622.iam.gserviceaccount.com
-// GOOGLE_CLIENT_ID=100396051393634173307
-// GOOGLE_AUTH_URI=https://accounts.google.com/o/oauth2/auth
-// GOOGLE_TOKEN_URI=https://oauth2.googleapis.com/token
-// GOOGLE_AUTH_PROVIDER_CERT_URL=https://www.googleapis.com/oauth2/v1/certs
-// GOOGLE_CLIENT_CERT_URL=https://www.googleapis.com/robot/v1/metadata/x509/munich-guide-bot%40testnode-296622.iam.gserviceaccount.com
 
 
 async function gameFound(playerName) {
